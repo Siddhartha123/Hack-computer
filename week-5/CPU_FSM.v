6108ec01249d40ac1741f91bf2b15489bc325596
Ippili Sidhartha Kumar <iskumar789@gmail.com>
2020-03-23 16:21:06 +0530

    modified loadPC to mealy output
    also removed unused parameters and wires

--- week-5/CPU_FSM.v
+++ week-5/CPU_FSM.v
@@ -10,23 +10,12 @@ reg [2:0] state;
 
 reg loadPC ;
 wire A_or_M, loadD, wM, PC_rst, PC_inc, A_or_C, loadA, dest_contains_A;
-reg [14:0] val;
+
 parameter STATE_RESET      = 0;
 parameter STATE_FETCH      = 1;
 parameter STATE_DECODE     = 2;
 parameter STATE_A_INS      = 3;
 parameter STATE_C_INS_COMP = 4;
-parameter STATE_C_INS_JMP  = 5;
-//word d1d2d3 decides a part of the control word {writeM,loadD}
-parameter dest_NULL = 1'b0;
-parameter dest_M    = 1'b0;
-parameter dest_D    = 1'b1;
-parameter dest_MD   = 1'b1;
-parameter dest_A    = 1'b0;
-parameter dest_AM   = 1'b0;
-parameter dest_AD   = 1'b1;
-parameter dest_AMD  = 1'b1;
-
 
 initial begin
     state = STATE_RESET;
@@ -35,52 +24,36 @@ end
 always @(posedge clk) begin
     if (reset)   state = STATE_RESET;
     else begin
-    case (state)
-        STATE_RESET: begin
-            /*
-             Initialize PC and state variables
-             */
-            loadPC   = 0;
-            state    = STATE_FETCH;
-        end
+        case (state)
+            STATE_RESET: begin
+                /*
+                Initialize PC and state variables
+                */
+                state    = STATE_FETCH;
+            end
+            
+            STATE_FETCH: begin
+                state  = STATE_DECODE;
+            end
+            
+            STATE_DECODE: begin
+                if (ins[15]) 		// C ins
+                state = STATE_C_INS_COMP;
+                else  						// A ins
+                state = STATE_A_INS;
+            end
+            
+            STATE_A_INS: begin  			//execute A ins
+                state    = STATE_FETCH;
+            end
         
-        STATE_FETCH: begin
-            state  = STATE_DECODE;
-        end
-        
-        STATE_DECODE: begin
-            if (ins[15]) 		// C ins
-            state = STATE_C_INS_COMP;
-            else  						// A ins
-            state = STATE_A_INS;
-        end
-        
-        STATE_A_INS: begin  			//execute A ins
-        loadPC   = 0;
-        state    = STATE_FETCH;
-    end
-    
-    STATE_C_INS_COMP: begin  			//execute C ins (computation)
-    
-    
-    case({ins[2:0],ALU_zr,ALU_ng})
-        5'b00100, //JGT
-        5'b01010, //JEQ
-        5'b01100,5'b01110, //JGE
-        5'b10001,    //JLT
-        5'b10100,5'b10101, //JNE
-        5'b11001,5'b11010, //JLE
-        5'b111??
-        : loadPC = 1;
-        default : loadPC = 0;
-    endcase
-    
-    state    = STATE_FETCH;
-end
+            STATE_C_INS_COMP: begin  			//execute C ins (computation)
+                state    = STATE_FETCH;
+            end
 
+            default: state = STATE_RESET;
 
-default: state = STATE_RESET;
-    endcase
+        endcase
     end
 end
 
@@ -98,6 +71,24 @@ assign loadD=(state==STATE_C_INS_COMP && (ins[5:3]==3'b010 || ins[5:3]==3'b011 |
 assign wM = (state==STATE_C_INS_COMP && ins[3]);
 assign PC_rst=(state==STATE_RESET);
 
+//procedural implementation of signal loadPC
+always @(ins[2:0],ALU_zr,ALU_ng,state) begin
+    // $display("{%b} jump detected, in state %d \n",{ins[2:0],ALU_zr,ALU_ng},state);
+    case({ins[2:0],ALU_zr,ALU_ng})
+        5'b00100, //JGT
+        5'b01010, //JEQ
+        5'b01100,5'b01110, //JGE
+        5'b10001,    //JLT
+        5'b10100,5'b10101, //JNE
+        5'b11001,5'b11010, //JLE
+        5'b11100,5'b11101,5'b11110,5'b11111 //JMP
+        : begin
+            loadPC <= (state==STATE_C_INS_COMP);
+            // $display("{%b} jump detected, in state %d \n",{ins[2:0],ALU_zr,ALU_ng},state);
+            end
+        default : loadPC <= 0;
+    endcase
+end
 assign ctrlWord = {A_or_C, A_or_M, loadA, loadD, PC_inc, loadPC, PC_rst};
 assign writeM   = wM;
 endmodule
