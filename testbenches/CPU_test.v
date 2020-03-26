de255ad05bd1e45f58b50c6ee675f353a2d6bf0a
Ippili Sidhartha Kumar <iskumar789@gmail.com>
2020-03-26 20:33:34 +0530

    synthesized memory of full size
    TODO: use the RAM and ROM modules designed before in place of always blocks

--- testbenches/CPU_test.v
+++ testbenches/CPU_test.v
@@ -9,29 +9,47 @@ module CPU_test;
     wire writeM;
     wire [6:0] control_word;
 
-    reg [15:0] RAM [31:0];
-    reg [15:0] ROM [31:0];
+    reg [15:0] RAM [32767:0];
+    reg [15:0] ROM [255:0];
 
+    reg [15:0] i;
     CPU uut (clk,reset,inM,instruction,outM,addressM,PC,writeM,control_word);
 
-    always #80 clk=~clk;
+    initial begin
+        clk=0;
+        for(i=0;i<11;i=i+1)
+            #80 clk=~clk;
+    end
 
     initial begin
-        $readmemb("/home/sid/others/Hack-computer/week-5/ROM_test.mem",ROM);
-        $readmemh("/home/sid/others/Hack-computer/week-5/RAM_test.txt",RAM);
-        clk=0;
+        $readmemb("/home/sid/Codes/nand2tetris/Hack-computer/week-5/SimpleFunction256.mem",ROM);
+        $readmemh("/home/sid/Codes/nand2tetris/Hack-computer/week-5/SimpleFunctionRAM32767.txt",RAM);
+        
+        RAM[0] = 317;
+        RAM[1] = 317;
+        RAM[2] = 310;
+        RAM[3] = 3000;
+        RAM[4] = 4000;
+        RAM[310] = 1234;
+        RAM[311] = 37;
+        RAM[312] = 1000;
+        RAM[313] = 305;
+        RAM[314] = 300;
+        RAM[315] = 3010;
+        RAM[316] = 4010;
+
         reset=1;
         #140;
         reset=0;
     end
 
-    assign inM = RAM[addressM[4:0]];
+    assign inM = RAM[addressM];
 
     always @(posedge clk) 
-        if(writeM) RAM[addressM[4:0]]=outM;
+        if(writeM) RAM[addressM]=outM;
 
     always @(posedge clk)
-        instruction=ROM[PC[4:0]];
+        instruction=ROM[PC[7:0]];
 
 
 
